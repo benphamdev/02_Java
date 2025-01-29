3c0d60ed9555caa7610095a3373e6fb05c16e0e8
Ben <duychien152003@gmail.com>
2025-01-29 16:23:14 +0700

    feat(template method): implement Network, Facebook, and Twitter classes for social media posting

---
+++ 03_DesignPattern/DesignPattern/src/Behavioral/template/networks/Network.java
@@ -0,0 +1,25 @@
+package Behavioral.template.networks;
+
+public abstract class Network {
+    String username;
+    String password;
+
+    Network() {}
+
+    // not implemented
+    public boolean post(String message) {
+        if (logIn(this.username, this.password)) {
+            boolean result = sendData(message.getBytes());
+            logOut();
+            return result;
+        }
+        return false;
+    }
+
+    // implemented in subclasses
+    abstract boolean logIn(String username, String password);
+
+    abstract void logOut();
+
+    abstract boolean sendData(byte[] bytes);
+}
