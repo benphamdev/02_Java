a968c80e4963c0870ad0826a54bb040c83dc2aea
Ben <duychien152003@gmail.com>
2025-01-30 21:28:57 +0700

    feat(state pattern): implement Player, UI, and state classes for media playback functionality

---
+++ 03_DesignPattern/DesignPattern/src/Behavioral/state/Demo.java
@@ -0,0 +1,12 @@
+package Behavioral.state;
+
+import Behavioral.state.ui.Player;
+import Behavioral.state.ui.UI;
+
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
---
+++ 03_DesignPattern/DesignPattern/src/Behavioral/state/states/LockedState.java
@@ -0,0 +1,34 @@
+package Behavioral.state.states;
+
+import Behavioral.state.ui.Player;
+
+public class LockedState extends State {
+    public LockedState(Player player) {
+        super(player);
+    }
+
+    @Override
+    public String onLock() {
+        if (player.isPlaying()) {
+            player.changeState(new ReadyState(player));
+            return "Stop playing";
+        } else {
+            return "Locked...";
+        }
+    }
+
+    @Override
+    public String onPlay() {
+        return "Ready...";
+    }
+
+    @Override
+    public String onNext() {
+        return "Locked...";
+    }
+
+    @Override
+    public String onPrevious() {
+        return "Locked...";
+    }
+}
