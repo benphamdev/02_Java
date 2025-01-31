a53d20debb5dbec72485a0d81f372720f43ba6bb
Ben <duychien152003@gmail.com>
2025-01-31 15:52:56 +0700

    feat(factory pattern): implement Dialog, and Button classes for UI component creation

---
+++ 03_DesignPattern/DesignPattern/src/creational/factory/Client.java
@@ -0,0 +1,26 @@
+package creational.factory;
+
+import creational.factory.factory.Dialog;
+import creational.factory.factory.HtmlDialog;
+import creational.factory.factory.WindowsDialog;
+
+public class Client {
+    private static Dialog dialog;
+
+    public static void main(String[] args) {
+        configure();
+        runBusinessLogic();
+    }
+
+    static void configure() {
+        if (System.getProperty("os.name").equals("Windows 11")) {
+            dialog = new WindowsDialog();
+        } else {
+            dialog = new HtmlDialog();
+        }
+    }
+
+    static void runBusinessLogic() {
+        dialog.renderWindow();
+    }
+}
---
+++ 03_DesignPattern/DesignPattern/src/creational/factory/buttons/Button.java
@@ -0,0 +1,7 @@
+package creational.factory.buttons;
+
+public interface Button {
+    void render();
+
+    void onClick();
+}
